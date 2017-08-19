ab5bdef5f7814ec7c6f8bce0416e3d5bcef586bf
Need2Revolt <francesco.davide.carnovale@gmail.com>
2017-08-20 00:03:00 +0200

    Adding a somewhat nice menu

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-var TelegramBot = require('node-telegram-bot-api'),
-    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-    telegram = new TelegramBot("BOT TOKEN", { polling: true });
+const TelegramBot = require('node-telegram-bot-api');
+// Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+const token = 'YOUR_BOT_TOKEN';
 
-telegram.on("text", (message) => {
-    telegram.sendMessage(message.chat.id, "I'm still learning to talk");
+const skyseerBot = new TelegramBot(token, {polling: true});
+
+//Main welcome message with menu
+skyseerBot.onText(/\/start/, (msg) => {
+  skyseerBot.sendMessage(msg.chat.id, "Insert witty welcome message here", {
+    "reply_markup": {
+        "keyboard": [["Predict"],["Astronomical night starting time"], ["Weather forecasts"], ["All options"]]
+        }
+    });
 });
+
+//reactions to main menu items
+skyseerBot.onText(/Predict/, (msg) => {
+  skyseerBot.sendMessage(msg.chat.id, "I can\'t see clearly right now");
+});
+
+skyseerBot.onText(/Astronomical night starting time/, (msg) => {
+  notImplementedYet(msg);
+});
+
+skyseerBot.onText(/Weather forecasts/, (msg) => {
+  notImplementedYet(msg);
+});
+
+skyseerBot.onText(/All options/, (msg) => {
+  notImplementedYet(msg);
+});
+
+//functions
+function notImplementedYet(msg) {
+  skyseerBot.sendMessage(msg.chat.id, "Not implemented yet");
+}
