081da5265d63cfb4f0cca23c106b52a0b6086d3a
Need2Revolt <francesco.davide.carnovale@gmail.com>
2017-08-20 17:38:56 +0200

    first feature is there and it's kind of working...

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const app     = express();
 
 //set an evironment variable with your bot token.
 // i.e. for Linux: export SKYSEER_TOKEN='YOUR_BOT_TOKEN'
@@ -15,7 +20,33 @@ skyseerBot.onText(/\/start/, (msg) => {
 
 //reactions to main menu items
 skyseerBot.onText(/Predict/, (msg) => {
-  skyseerBot.sendMessage(msg.chat.id, "I can\'t see clearly right now");
+  //of course i need this to be dynamic...
+  url = 'https://www.timeanddate.com/astronomy/italy/cagliari';
+
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
+  request(url, function(error, response, html){
+
+      // First we'll check to make sure no errors occurred when making the request
+      if(!error){
+          // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+          var $ = cheerio.load(html);
+
+          //Parse what we need to parse
+          var rawTextEnd = $('.tr', '#lm-leg-0').text();
+          var rawTextStart = $('.tr', '#lm-leg-8').text();
+
+          // Finally, we'll define the variables we're going to capture
+          var astroNightTimeStart, astroNightTimeEnd;
+          astroNightTimeStart = rawTextStart.split("–", 1); //that is not a dash, it's some lame ass character
+          astroNightTimeEnd = rawTextEnd.split("–").pop(); //that is not a dash, it's some lame ass character
+
+          var message = `Astronomical night *starts at ${astroNightTimeStart}* and ends the day after at *${astroNightTimeEnd}*`;
+          skyseerBot.sendMessage(msg.chat.id, message, {parse_mode: "Markdown"});
+      }
+  })
+
 });
 
 skyseerBot.onText(/Astronomical night starting time/, (msg) => {
